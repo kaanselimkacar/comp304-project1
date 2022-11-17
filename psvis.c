14d50e26cf6be0efb9ba8f42e2a60744f1f6ee37
bkulek20 <72880656+bkulek20@users.noreply.github.com>
2022-11-17 16:44:56 +0300

    psvis update takes inputs

--- psvis.c
+++ psvis.c
@@ -15,8 +15,8 @@
 #include <fcntl.h>
 
 int main(int argc, char* argv[]){
-	//printf("argv[0]: %s\n",argv[0]);
-	//printf("argv[1]: %s\n",argv[1]);
-	//printf("argv[2]: %s\n",argv[2]);
+	char *PID = argv[1];
+	char *output_txt = argv[2];
+	//printf("PID: %s\noutput_txt: %s\n",argv[1],argv[2]);
 	
 }
